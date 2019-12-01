c28bdc89bac51d0dc95e4c9800171dc3757f25e8
dyq666 <694526892@qq.com>
2019-12-01 19:23:33 +0800

    opt: use `Binary.bytes_xor` in `OneTimePad`

--- nami/symmetric.py
+++ nami/symmetric.py
@@ -1,41 +1,4 @@
-"""对称密码
-
-## XOR
-
-英文: exclusive or, 中文: 异或. 公式如下.
-
-```
-    0 XOR 0 = 0
-    0 XOR 1 = 1
-    1 XOR 0 = 1
-    1 XOR 1 = 0
-```
-
-异或操作可以看成黑白棋的翻转. 假设 0 是白, 1 是黑, 棋子被翻转则换颜色.
-又假设操作 0 是不翻转, 操作 1 是翻转. 因此上面的公式可以这样解释.
-
-```
-    白 不翻转 = 白
-    白 翻转 = 黑
-    黑 不翻转 = 黑
-    黑 翻转 白
-```
-
-## 一次性密码本
-
-一次性密码就是利用 XOR 对数据进行加密和解密. 假设我们将数据 A 和数据 B 做两次 XOR,
-A XOR B XOR B. 也就是等价于将每位都翻转两次或不翻转两次, 最终结果就是 A 没变.
-第一次 XOR 可以看做加密, 而第二次 XOR 可以看做解密, B 则是密钥.
-
-如果选取了合适的密钥, 并且密钥没有泄露, 那么一次性密码是永远无法破解的. 假设明文是 `midnight`,
-破译者使用暴力破解法对密文解密, 解密过程中会出现 `midnight` 这个词, 但同时也会出现相同位数的任意词,
-例如 `mistress` 等, 破解者无法得知哪个参数真正的明文.
-
-但是一次性密码本有很大的弊端, 导致没有被使用. XOR 是按位加密, 假设明文有 1024 位, 那么密钥
-也得有 1024 位, 当然也可以加密一部分数据, 例如只加密其中的 512 位, 但加密位数越少越可能被破解,
-所以实际上密钥的位数也不差明文太多. 我们用密钥加密了明文, 现在要发送密钥, 但密钥和明文大小差不多,
-也就是如果我们有办法安全的发送一个密钥, 那么我们就有办法安全的发送一个和密钥大小差不多的明文, 那么
-就不需要密钥了.
+"""对称密码.
 
 ### Feistel 网络
 
@@ -119,23 +82,23 @@ __all__ = (
 import secrets
 from typing import Tuple
 
+from util import Binary
+
 
 class OneTimePad:
+    """一次性密码本.
 
-    """一次性密码本
-    以字节为单位进行加密解密, 因为一个字节的大小是 [0, 256), 所以密钥的大小也是这个.
+    其中 key 的长度等于明文的长度.
     """
 
     @staticmethod
-    def encrypt(plaintext: bytes) -> Tuple[bytes, bytes]:
-        key = bytes([secrets.choice(range(0, 256)) for _ in range(len(plaintext))])
-        ciphertext = bytes([byte ^ key[i] for i, byte in enumerate(plaintext)])
-        return ciphertext, key
+    def encrypt(msg: bytes) -> Tuple[bytes, bytes]:
+        key = bytes([secrets.choice(range(0, 256)) for _ in range(len(msg))])
+        return Binary.bytes_xor(key, msg), key
 
     @staticmethod
-    def decrypt(ciphertext: bytes, key: bytes) -> bytes:
-        plaintext = bytes([byte ^ key[i] for i, byte in enumerate(ciphertext)])
-        return plaintext
+    def decrypt(msg: bytes, key: bytes) -> bytes:
+        return Binary.bytes_xor(key, msg)
 
 
 class Feistel:
