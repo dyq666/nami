9ef5082c6b258d019e0ce064fbef4f24814c7ebc
dyq666 <694526892@qq.com>
2019-12-01 10:28:18 +0800

    feature: add `CBC` and `ECB` modes in `AES`

--- nami/block_mode.py
+++ nami/block_mode.py
@@ -1,84 +1,42 @@
 """分组密码的模式
 
-只能加密固定长度的算法, 如果要变成加密任意长度的算法, 就需要选择一种迭代的模式.
-例如对称加密中的 DES, AES 就是这种, 统称分组密码. 与之相反的就是流密码例如一次性密码本.
+ECB (Electronic CodeBook) 模式, 所有明文分组独自加密.
 
-## ECB 模式 (Electronic CodeBook)
-
-流程如下, 每个明文分组都分开加密, 由于使用的算法和密钥都是一样的, 因此
-相同的明文分组会得到相同的密文分组. 攻击者可以根据密文分组出现的频率来获取
-一些明文的线索.
-
-```
-明文分组1   明文分组2   明文分组3
-
-   ↓ 加密    ↓ 加密     ↓ 加密
-
-密文分组1   密文分组2   密文分组3
-```
-
-## CBC 模式 (Cipher Block Chaining)
-
-流程如下, 每个明文分组都需要与前一个密文 XOR 后再加密.
-
-```
-         明文分组1        明文分组2     明文分组3
-
-   ↑   →   ↓ XOR   ↑  →  ↓ XOR   ↑  →  ↓ XOR
-
-   ↑       ↓ 加密   ↑     ↓ 加密   ↑     ↓ 加密
-
-初始化向量     密文分组1      密文分组2     密文分组3
-
-初始化向量实际上就是一个虚假的密文分组, 需要和密文分组保持一致的大小. 因此在 AES
-中初始化向量应该是 16 字节.
-
-CBC 模式下, 任意一个明文分组不正确, 都会影响后面所有的解密.
+CBC (Cipher Block Chaining) 模式, 前一个密文分组与当前的明文分组 XOR 后再加密.
 ```
 """
 
 __all__ = (
-    'AESMode',
+    'AES',
 )
 
 import secrets
 
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
 
-from util import fill_seq
+backend = default_backend()
 
 
-class AESMode:
+class AES:
+    """AES 加密, 解密."""
 
-    def __init__(self, key: bytes):
-        self.key = key
+    BLOCK_SIZE = 16
 
-    def ecb_encrpty(self, plaintext: bytes) -> bytes:
-        mode = modes.ECB()
-        return self._encrypt(plaintext, mode)
-
-    def cbc_encrpty(self, plaintext: bytes, iv: bytes) -> bytes:
-        mode = modes.CBC(iv)
-        return self._encrypt(plaintext, mode)
-
-    def _encrypt(self, plaintext: bytes, mode) -> bytes:
-        plaintext = self.filler(plaintext)
-        cipher = Cipher(
-            algorithm=algorithms.AES(self.key),
+    def __init__(self, mode, key: bytes = None):
+        key = secrets.token_bytes(32) if key is None else key
+        self.cipher = Cipher(
+            algorithm=algorithms.AES(key),
             mode=mode,
-            backend=default_backend()
+            backend=backend,
         )
-        encryptor = cipher.encryptor()
-        return encryptor.update(plaintext) + encryptor.finalize()
 
-    @staticmethod
-    def filler(text: bytes) -> bytes:
-        # aes 加密的明文字节数必须能被 16 整除
-        bytes_size = algorithms.AES.block_size // 8
-        return fill_seq(text, size=bytes_size, filler=b'\x01')
+    def encrypt(self, msg: bytes) -> bytes:
+        """加密."""
+        encryptor = self.cipher.encryptor()
+        return encryptor.update(msg) + encryptor.finalize()
 
-    @staticmethod
-    def gen_key() -> bytes:
-        # aes 密钥长度只能是 16, 24, 32, (单位: 字节)
-        return secrets.token_bytes(32)
+    def decrypt(self, msg: bytes) -> bytes:
+        """解密."""
+        decryptor = self.cipher.decryptor()
+        return decryptor.update(msg) + decryptor.finalize()
--- test/test_block_mode.py
+++ test/test_block_mode.py
@@ -1,28 +1,48 @@
 import secrets
 
-from nami.block_mode import AESMode
+import pytest
+from cryptography.hazmat.primitives.ciphers import modes
+
+from nami.block_mode import AES
+from util import Binary
 
 
 class TestAESMode:
 
     def test_ecb_mode(self):
-        key = AESMode.gen_key()
-        aes = AESMode(key)
-        plaintext = b'1' * 32
-        ciphertext = aes.ecb_encrpty(plaintext)
-        # 两组内容相同都是 b'1....', 加密后相同.
-        assert ciphertext[:len(ciphertext) // 2] == ciphertext[len(ciphertext) // 2:]
+        aes = AES(modes.ECB())
+
+        # 如果明文分组相同, 那么密文分组也相同.
+        msg = b'1' * AES.BLOCK_SIZE * 2
+        ciphertext = aes.encrypt(msg)
+        assert ciphertext[:AES.BLOCK_SIZE] == ciphertext[AES.BLOCK_SIZE:]
+        assert ciphertext[:AES.BLOCK_SIZE] == aes.encrypt(b'1' * AES.BLOCK_SIZE)
+
+        # `msg` 的长度必须是 `AES.BLOCK_SIZE` 的倍数.
+        msg = b'1'
+        with pytest.raises(ValueError):
+            aes.encrypt(msg)
 
     def test_cbc_mode(self):
-        key = AESMode.gen_key()
-        iv = secrets.token_bytes(16)
-        aes = AESMode(key)
+        iv = secrets.token_bytes(AES.BLOCK_SIZE)
+        key = secrets.token_bytes(32)
 
-        plain_l, plain_r = b'1' * 16, b'1' * 16
-        ciphertext = aes.cbc_encrpty(plain_l + plain_r, iv)
-        cipher_l, cipher_r = ciphertext[:len(ciphertext) // 2], ciphertext[len(ciphertext) // 2:]
-        # 两组内容相同都是 b'1....', 加密后不同.
-        assert cipher_l != cipher_r
+        # 明文分组相同, 密文分组不同.
+        aes = AES(modes.CBC(iv), key=key)
+        plain_1, plain_2 = b'1' * AES.BLOCK_SIZE, b'1' * AES.BLOCK_SIZE
+        ciphertext = aes.encrypt(plain_1 + plain_2)
+        cipher_1, cipher_2 = ciphertext[:AES.BLOCK_SIZE], ciphertext[AES.BLOCK_SIZE:]
+        assert cipher_1 != cipher_2
+        # 第一组加密的结果作为第二组的 iv 对第二组加密, 和两组一起加密的结果相同.
+        aes = AES(modes.CBC(cipher_1), key=key)
+        assert aes.encrypt(plain_1) == cipher_2
 
-        # 第一组加密的结果作为第二组的 iv 对第二组加密, 和两组一起加密的结果相同
-        assert aes.cbc_encrpty(plain_r, cipher_l) == cipher_r
+        # `msg` 的长度必须是 `AES.BLOCK_SIZE` 的倍数.
+        msg = b'1'
+        with pytest.raises(ValueError):
+            aes.encrypt(msg)
+
+        # 先与前一组 xor 后加密.
+        msg = b'1' * AES.BLOCK_SIZE
+        msg1 = Binary.bytes_xor(msg, iv)
+        assert AES(modes.CBC(iv), key=key).encrypt(msg) == AES(modes.ECB(), key=key).encrypt(msg1)
