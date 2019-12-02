c03c81e43dd4a8af0c291033bc35175dcfd97c03
dyq666 <694526892@qq.com>
2019-12-02 19:18:55 +0800

    feature: opt base64 implement

--- nami/base64_.py
+++ nami/base64_.py
@@ -1,9 +1,9 @@
 """
 ## 个人理解
 
-base64 最初用于解决邮件无法识别非 ascii 的字节 (7 bit). 具体方式是增加数据冗余, 将 8 bit
-的表示形式转换成 6 bit + 填充 0 的方式, 当然转换更少 bit 的算法, 转化的位数越低, 数据相较之前的数据
-就越大. 从这个角度上看可能 base128 更优秀, 但 ascii 中有一部分控制字符, 控制字符不易读, 因此
+base64 最初用于解决邮件无法识别非 ascii 的字节 (前 127 个). 具体方式是增加数据冗余, 将 8 bit
+的表示形式转换成 6 bit + 填充 0 的方式, 当然也可以转换成更少 bit 的算法, 转化的位数越低, 编码后数据长度就越大.
+从这个角度上看可能 base128 更优秀, 但 ascii 中有一部分控制字符, 控制字符不易读, 因此
 最常用的还是 base64. base64 中选取了 64 个可读的字符, 这里比较可惜的是, 大小字母 + 数组只有 62 个,
 因此又选了 `+/` 这两个字符, 这个选择也导致了一些问题. 如果 `+/` 在一些数据中是类似 `关键字` 的存在,
 那么就不能直接使用标准的 base64, 例如 url.
@@ -15,27 +15,27 @@ https://stackoverflow.com/questions/4080988/why-does-base64-encoding-require-pad
 
 ## base64 编码解码核心过程 (不包含末尾补 '=' 的逻辑):
 
-8
+8 bits
 
 ↓ 末尾补 '0' 使整个字符串可以被 6 整除
-  a c -> 01100001 01100011 -> 01100001 01100011 00 -> 011000 010110 001100)
+  a c -> 01100001 01100011 -> 01100001 01100011 00 -> 011000 010110 001100
 
-6
+6 bits
 
 ↓ 每个字节头部加两个 0, 按表转换
   011000 010110 001100 -> 00011000 00010110 00001100 -> Y W M
 
-8
+8 bits (上面是 encode, 下面是 deocde)
 
 ↓ 按表转换, 每个字节头部删去两个 0
   Y W M -> 00011000 00010110 00001100 -> 011000 010110 001100
 
-6
+6 bits
 
 ↓ 删除用于补齐的 '0' (等价于让整个字符串可以被 8 整除)
   011000 010110 001100 -> 01100001 01100011 00 -> 01100001 01100011 ->  a c
 
-8
+8 bits
 """
 
 __all__ = (
@@ -46,7 +46,7 @@ __all__ = (
 
 from string import ascii_uppercase, ascii_lowercase, digits
 
-from util import fill_seq, seq_grouper
+from util import Binary, fill_seq, seq_grouper, strip_seq
 
 B64_CHARS = ascii_uppercase + ascii_lowercase + digits + '+/'
 # 6 位二进制 -> char, 表中记录二进制比十进制在 base64 的编码解码过程中更加方便 (不是通过理论得出的, 仅从 Python 编写代码的角度上考虑)
@@ -55,23 +55,15 @@ B64_DECODE_MAP = {char: i for i, char in B64_ENCODE_MAP.items()}
 
 
 def b64encode(s: bytes) -> bytes:
-    # 二进制字符串
-    bin_str = ''.join(format(b, '08b') for b in s)
-    # 二进制字符串分组, 每组 6 个, 不足用 '0' 补齐
-    six_group = (str_ for str_ in seq_grouper(bin_str, 6, '0'))
-    # 按 base64 表转换
-    b64_chars = ''.join(B64_ENCODE_MAP[i] for i in six_group)
-    b64_chars = fill_seq(b64_chars, 4, '=')
-    return b64_chars.encode('ascii')
+    bin_str = Binary.bytes_2_str(s)
+    # 6 个一组, 不足用 '0' 补齐, 分组后按 sbase64 表转换
+    b64_chars = ''.join(B64_ENCODE_MAP[i] for i in seq_grouper(bin_str, 6, '0'))
+    return fill_seq(b64_chars, 4, '=').encode('ascii')
 
 
 def b64decode(s: bytes) -> bytes:
-    b64_chars = s.decode('ascii')
-    b64_chars = b64_chars.rstrip('=')
-    # 按 base64 表转换
+    b64_chars = s.decode('ascii').rstrip('=')
     bin_str = ''.join(B64_DECODE_MAP[char] for char in b64_chars)
-    # 二进制字符串分组, 每组 8 个, 余下部分删除 (余下的部分实际上编码过程中补充的 '0')
-    if len(bin_str) % 8 != 0:
-        bin_str = bin_str[:-(len(bin_str) % 8)]
-    eight_group = (str_ for str_ in seq_grouper(bin_str, 8))
-    return bytes(int(i, 2) for i in eight_group)
+    # 8 个一组, 多余部分删除 (多余的部分实际上 `b64encode` 过程中补充的 '0')
+    bin_str = strip_seq(bin_str, 8)
+    return Binary.str_2_bytes(bin_str)
