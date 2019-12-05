836311e7a30b49249e0456693f4d91d5ed073b12
dyq666 <694526892@qq.com>
2019-12-05 17:37:40 +0800

    opt: make `key` and `iv` to fixtures

--- nami/block_mode.py
+++ nami/block_mode.py
@@ -1,24 +1,11 @@
-"""分组密码的模式
+"""分组密码的模式.
 
-ECB (Electronic CodeBook), 所有明文分组独自加密.
 
-CBC (Cipher Block Chaining), 前一个密文分组与当前的明文分组 XOR 后再加密.
-
-CFB (Cipher FeedBack), 前一个密文分组加密后再与当前的明文分组 XOR.
-
-OFB (Output FeedBack), 从 iv 开始无限加密, 每次加密后的结果与明文分组 XOR.
-
-CTR (Counter), 每次加密计数器后的结果与明文分组 XOR.
-
-目前只有 ECB 是不能被使用的, 其他均可使用, 但使用最多的是 CBC.
-```
 """
 
 __all__ = (
     'AES',
 )
-
-import secrets
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
@@ -31,8 +18,7 @@ class AES:
 
     BLOCK_SIZE = 16
 
-    def __init__(self, mode, key: bytes = None):
-        key = secrets.token_bytes(32) if key is None else key
+    def __init__(self, mode, key: bytes):
         self.cipher = Cipher(
             algorithm=algorithms.AES(key),
             mode=mode,
--- test/test_block_mode.py
+++ test/test_block_mode.py
@@ -9,10 +9,18 @@ from nami.util import Binary
 
 class TestAESMode:
 
-    def test_ecb_mode(self):
-        aes = AES(modes.ECB())
+    @pytest.fixture
+    def key(self):
+        return secrets.token_bytes(32)
 
-        # 如果明文分组相同, 那么密文分组也相同.
+    @pytest.fixture
+    def iv(self):
+        return secrets.token_bytes(AES.BLOCK_SIZE)
+
+    def test_ecb_mode(self, key):
+        aes = AES(modes.ECB(), key)
+
+        # 如果明文分组相同, 那么密文分组也相同, 并且和单独加密明文分组的值一样.
         msg = b'1' * AES.BLOCK_SIZE * 2
         ciphertext = aes.encrypt(msg)
         assert ciphertext[:AES.BLOCK_SIZE] == ciphertext[AES.BLOCK_SIZE:]
@@ -23,10 +31,7 @@ class TestAESMode:
         with pytest.raises(ValueError):
             aes.encrypt(msg)
 
-    def test_cbc_mode(self):
-        iv = secrets.token_bytes(AES.BLOCK_SIZE)
-        key = secrets.token_bytes(32)
-
+    def test_cbc_mode(self, key, iv):
         # 明文分组相同, 密文分组不同.
         aes = AES(modes.CBC(iv), key=key)
         plain_1, plain_2 = b'1' * AES.BLOCK_SIZE, b'1' * AES.BLOCK_SIZE
@@ -47,9 +52,9 @@ class TestAESMode:
         msg1 = Binary.bytes_xor(msg, iv)
         assert AES(modes.CBC(iv), key=key).encrypt(msg) == AES(modes.ECB(), key=key).encrypt(msg1)
 
-    def test_cfb(self):
+    def test_cfb_mode(self, key, iv):
         # `msg` 的长度可以是任意长度
-        aes = AES(modes.CFB(secrets.token_bytes((AES.BLOCK_SIZE))))
+        aes = AES(modes.CFB(iv), key)
         assert aes.encrypt(b'1')
 
         # 前一组先加密再 xor.
@@ -63,9 +68,9 @@ class TestAESMode:
         assert cipher1 == Binary.bytes_xor(ecb.encrypt(iv), msg1)
         assert cipher2 == Binary.bytes_xor(ecb.encrypt(cipher1), msg2)
 
-    def test_ofb(self):
+    def test_ofb_mode(self, key, iv):
         # `msg` 的长度可以是任意长度
-        aes = AES(modes.OFB(secrets.token_bytes((AES.BLOCK_SIZE))))
+        aes = AES(modes.OFB(iv), key)
         assert aes.encrypt(b'1')
 
         # 前一组先加密再 xor.
