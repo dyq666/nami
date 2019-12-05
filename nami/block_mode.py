a52e545c02211d1a067748bde92c9781310c8c66
dyq666 <694526892@qq.com>
2019-12-05 19:24:20 +0800

    opt: change doc of `block_mode`

--- nami/block_mode.py
+++ nami/block_mode.py
@@ -1,11 +1,11 @@
 """分组密码的模式.
 
 demo:
-  - P79 ECB 模式. `test/test_block_mode/test_ecb_mode`
-  - P82 CBC 模式. `test/test_block_mode/test_cbc_mode`
-  - P86 小测验 4 仿 CBC 模式. `test/test_block_mode/test_fake_cbc_mode`
-  - P88 CFB 模式. `test/test_block_mode/test_cfb_mode`
-  - P91 OFB 模式. `test/test_block_mode/test_ofb_mode`
+  - P79 ECB 模式. `test/test_block_mode.py::test_ecb_mode`
+  - P82 CBC 模式. `test/test_block_mode.py::test_cbc_mode`
+  - P86 小测验 4 仿 CBC 模式. `test/test_block_mode.py::test_fake_cbc_mode`
+  - P88 CFB 模式. `test/test_block_mode.py::test_cfb_mode`
+  - P91 OFB 模式. `test/test_block_mode.py::test_ofb_mode`
 
 比较推荐的两种模式是 CBC 和 CTR. AES_CBC 和 AES_CTR 两种的详细实现在:
 https://github.com/dyq666/sanji/blob/master/util/third_cryptography.py
